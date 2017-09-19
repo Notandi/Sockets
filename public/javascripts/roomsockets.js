d1f2e01b4d4c6a5e3cc8471f7ea5de6c85abed35
Notandi <oskarola7@gmail.com>
2017-09-19 14:38:31 +0000

    Collaborative text editing with websockets works now

--- public/javascripts/roomsockets.js
+++ public/javascripts/roomsockets.js
@@ -16,8 +16,30 @@ window.onload= function(){
     ws.send(JSON.stringify(data));
     //socket.emit("insertTextAt", data);
   });
-
+  ws.onmessage = ( (message) => {
+    console.log(message.data);
+    data = JSON.parse(message.data);
+    key = data.key;
+    line = data.line;
+    ch = data.ch;
+    if (key.length > 1){
+      if (key === "Enter"){
+        key = '\n';
+        textArea.replaceRange(key, {line, ch});
+      }
+      if (key === "Backspace"){
+        textArea.replaceRange("", {line, ch: ch +1}, {line, ch});
+      }
+      if (key === "Delete"){
+        textArea.replaceRange("", {line, ch},{line, ch : ch + 1});
+      }
+    } else {
+      textArea.replaceRange(key, {line, ch});
+    }
+  })
 }
+
+
 /*
 socket.on('insertTextAt', function (data) {
   console.log(data);
